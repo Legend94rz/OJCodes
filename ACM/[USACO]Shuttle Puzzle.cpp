36459b2cddf90cf6520ec39a3e05f6f0a408bf2d
legend <Legend94rz@gmail.com>
2017-10-25 10:54:14 +0800

    usa shuttle puzzle tmp

---
+++ ACM/[USACO]Shuttle Puzzle.cpp
@@ -0,0 +1,25 @@
+/*
+ID: rz109291
+PROG: shuttle
+LANG: C++
+*/
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int N;
+void dfs(int state,byte p)
+{
+	
+	
+}
+int main()
+{
+	freopen("shuttle.in","r",stdin);freopen("shuttle.out","w",stdout);
+	cin>>N;
+	
+	dfs( ((1<<N)-1))<<(N+1) ,N/2+1);
+	
+	
+	fclose(stdin);fclose(stdout);
+}
