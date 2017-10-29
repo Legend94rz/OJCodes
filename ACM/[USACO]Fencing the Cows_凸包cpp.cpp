1c91246ef0389515894fe92e8a77761b4b99de63
legend <Legend94rz@gmail.com>
2017-10-29 21:30:43 +0800

    usa fencing WA

---
+++ ACM/[USACO]Fencing the Cows_凸包cpp.cpp
@@ -0,0 +1,95 @@
+/*
+ID: rz109291
+PROG: fc
+LANG: C++
+*/
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int N;
+double x[10001], y[10001], alg[1000];
+int perm[10001];
+double midx, midy;
+int stack[10001];
+int top,head;
+void Qsort(int s, int t)
+{
+	int i = s, j = t;
+	double k = alg[(s + t) >> 1];
+	int u;
+	double v;
+	while (i<=j)
+	{
+		while (alg[i] < k)i++;
+		while (alg[j] > k)j--;
+		if (i <= j)
+		{
+			u = perm[i]; perm[i] = perm[j]; perm[j] = u;
+			v = alg[i]; alg[i] = alg[j]; alg[j] = v;
+			i++; j--;
+		}
+	}
+	if (i < t)Qsort(i, t);
+	if (s < j)Qsort(s, j);
+}
+bool checkTop3(int t1,int t2,int t3)
+{
+	double v1[2], v2[2];
+	v1[0] = x[t2] - x[t1]; v1[1] = y[t2] - y[t1];
+	v2[0] = x[t3] - x[t2]; v2[1] = y[t3] - y[t2];
+	return (v1[0] * v2[1] - v1[1] * v2[0]) > 0;
+}
+double dist(int i, int j)
+{
+	return sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2));
+}
+int main()
+{
+	//freopen("fc.in", "r", stdin); freopen("fc.out", "w", stdout);
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x[i] >> y[i];
+		midx = midx + x[i]/N;
+		midy = midy + y[i]/N;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		alg[i] = atan2(y[i] - midy, x[i] - midx);
+		perm[i] = i;
+	}
+	Qsort(0,N-1);
+	stack[top++] = perm[0];
+	stack[top++] = perm[1];
+	for (int i = 2; i < N; i++)
+	{
+		while (top>=2 && !checkTop3(stack[top-2],stack[top-1],perm[i]))
+		{
+			top--;
+		}
+		stack[top++] = perm[i];
+	}
+	for (bool f=true;f;)
+	{
+		f = false;
+		if (top-head>=2 && !checkTop3(stack[top - 1], stack[head], stack[head+1]))
+		{
+			head++;
+			f = true;
+		}
+		if (top - head >= 2 && !checkTop3(stack[top - 2], stack[top - 1], stack[head]))
+		{
+			top--;
+			f = true;
+		}
+	}
+	double ans = 0;
+	for (int i = head+1; i < top; i++)
+	{
+		ans += dist(stack[i],stack[i-1]);
+	}ans += dist(stack[top - 1], stack[head]);
+	printf("%.2lf\n",ans);
+	fclose(stdin); fclose(stdout);
+}
